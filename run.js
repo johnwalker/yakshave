3a8243d2a32c5988ceec711e77b46bc9b8af640c
John Walker <john.lou.walker@gmail.com>
2014-08-30 17:36:30 -0700

    Init

---
+++ run.js
@@ -0,0 +1,9 @@
+try {
+    require("source-map-support").install();
+} catch(err) {
+}
+require("./out/goog/bootstrap/nodejs.js");
+require("./out/io/johnwalker/yakshave.js");
+require("./out/io/johnwalker_SLASH_yakshave/core.js");
+goog.require("io.johnwalker.yakshave.core");
+goog.require("cljs.nodejscli");
