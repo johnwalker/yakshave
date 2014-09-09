fb93fa4b754d75d06d23c467af59f294ec9cb9f1
John Walker <john.lou.walker@gmail.com>
2014-09-09 08:54:43 -0700

    Upgrade deps

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2311"]]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
+                 [org.clojure/clojurescript "0.0-2322"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
